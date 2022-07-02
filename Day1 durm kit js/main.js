8a1149bcc463cc3bbfae61c871d27fabc7bfeb7a
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-07-02 21:18:35 +0530

    day1 completed

---
+++ Day1 durm kit js/main.js
@@ -0,0 +1,14 @@
+// const mainElement = document.querySelector(".main");
+// const keyElement = document.querySelectorAll(".keys")
+
+// function removeTransition() {
+//     keyElement.classList.add("playing");
+//     // mainElement.classList.remove("playing");
+// }
+
+
+// keyElement.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', function(e) {
+    console.log(e);
+});
